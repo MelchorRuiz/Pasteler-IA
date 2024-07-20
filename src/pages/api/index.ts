539852392335371a45e67574542f183a86face90
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-07-20 15:45:19 -0600

    chore: Add index.astro and /api/index.ts

---
+++ src/pages/api/index.ts
@@ -0,0 +1,21 @@
+import type { APIRoute } from "astro";
+import { createOpenAI } from '@ai-sdk/openai';
+import { generateText } from 'ai';
+
+const perplexity = createOpenAI({
+    apiKey: import.meta.env.PERPLEXITY_API_KEY ?? '',
+    baseURL: 'https://api.perplexity.ai/',
+});
+
+const model = perplexity('llama-3-sonar-large-32k-online');
+
+export const GET: APIRoute = async() => {
+    const { text } = await generateText({
+        model,
+        prompt: 'Escribe un mensaje de bienvenida cálido y amigable para un nuevo usuario, utilizando exactamente 20 palabras, que invite a explorar y participar.',
+    });
+    return new Response(JSON.stringify({
+        message: text
+    })
+    )
+}
