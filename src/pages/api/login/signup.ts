e35bccab79c71c910341f395951fae1dd4b4fdf6
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-07-29 14:53:18 -0600

    refactor: Remove unused Session collection in signup API

--- src/pages/api/login/signup.ts
+++ src/pages/api/login/signup.ts
@@ -11,19 +11,12 @@ interface UserDoc {
   password_hash: string;
 }
 
-interface SessionDoc {
-	_id: string;
-	expires_at: Date;
-	user_id: string;
-}
-
 const uri = import.meta.env.MONGODB_URI;
 const client = new MongoClient(uri);
 await client.connect();
 
 const db = client.db('pasteler-ia');
 const User = db.collection("users") as Collection<UserDoc>;
-const Session = db.collection("sessions") as Collection<SessionDoc>;
 
 export async function POST(context: APIContext): Promise<Response> {
 	const formData = await context.request.formData();
