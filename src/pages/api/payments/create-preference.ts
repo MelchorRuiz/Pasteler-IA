54c54ec1d668d83e55f1c81b0debc6a696bf75c2
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-08-01 11:01:18 -0600

    feat: Add mercadopago checkout

---
+++ src/pages/api/payments/create-preference.ts
@@ -0,0 +1,63 @@
+import type { APIRoute } from "astro";
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+import { MongoClient, ObjectId } from 'mongodb';
+
+interface Product {
+  id: string;
+  quantity: number;
+}
+
+const uri = import.meta.env.MONGODB_URI;
+const clientDB = new MongoClient(uri);
+const database = clientDB.db('pasteler-ia');
+const cakes = database.collection('cakes');
+
+const client = new MercadoPagoConfig({
+  accessToken: import.meta.env.MERCADOPAGO_ACCESS_TOKEN,
+});
+
+export const POST: APIRoute = async ({ request }) => {
+  const { products } = await request.json();
+
+  if (!Array.isArray(products) || products.length === 0) {
+    return new Response(JSON.stringify({ error: 'Invalid products' }),
+      { headers: { "content-type": "application/json" }, status: 400 }
+    );
+  }
+
+  const results = products.map(async (product: Product) => {
+    const cake = await cakes.findOne({ _id: new ObjectId(product.id) });
+    if (!cake) {
+      return {
+        id: product.id,
+        title: 'Unknown',
+        quantity: product.quantity,
+        unit_price: 0,
+        description: 'Unknown product'
+      };
+    }
+
+    return {
+      id: product.id,
+      title: cake.name,
+      quantity: product.quantity,
+      unit_price: parseInt(cake.price),
+      description: cake.description
+    };
+  });
+
+  const items = await Promise.all(results);
+  const preference = new Preference(client);
+
+  const response = await preference.create({
+    body: {
+      items
+    }
+  })
+
+  console.log(response.id);
+  return new Response(JSON.stringify({ id: response.id }),
+    { headers: { "content-type": "application/json" }, status: 200 }
+  );
+
+}
